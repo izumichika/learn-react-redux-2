ff4be47bb292ce4b4a0b4845a3495d7032f9f987
izumichika <chikara0707@outlook.jp>
2020-05-14 15:26:10 +0900

    change text by terminal

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
-
 function App() {
-  return (<div><h1>Hello World!</h1></div>  );
+  return(<h1>Hello World!</h1>  );
 }
 
 export default App;
+
+// VScodeでターミナルを使いたい時「control + shift　+　`を実行。」
+
+// 文字置換
+// gsed -i "" -e "s/div/h1/g" src/App.js
+// -i は上書き保存するオプション ( --in-place ) -e はスクリプトを実行してバックアップを作成するオプション (--expression=script) バックアップ作成は強制されてしまうのですが -i ""とすることでバックアップファイルを作らずにコマンドを実行することができます
