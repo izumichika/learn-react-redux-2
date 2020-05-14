ef0484e9fdef0b859e7af6960e7c95b30c6b1f08
izumichika <chikara0707@outlook.jp>
2020-05-14 22:21:09 +0900

    learn state and set state

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-import React　from 'react';
-import PropTypes from 'prop-types';
+import React, { Component }　from 'react';
 
-const App = () => {
-  const profiles = [
-    {name: "Taro" , age: 10} , 
-    {name: "Hanako", age: 5},
-    {name: "noName", age: 2},
+const App = () => (<Counter></Counter>)
+class Counter extends Component {
+  constructor(props){
+    super(props)
+    this.state = { count: 0 }
+  }
+  handlePlusButton = () => {
+    this.setState({ count: this.state.count + 1})
+  }
+  handleMinusButton = () => {
+    this.setState({ count: this.state.count -1 })
+  }
+  render(){
+    return (
+      <React.Fragment>
+        <div>counter: {this.state.count}</div>
+        <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.handleMinusButton}>-1</button>
+      </React.Fragment>
+
     
-  ]
-  return(
-    <div>
-      {
-        profiles.map((profile, index) => {
-          return <User name={profile.name} age={profile.age} key={index}/>
-        })
-      }
-    </div>
-  )
-}
-const User = (props) => {
-return <div>Hi, I am {props.name}, and {props.age} years old.</div> 
+    )
+  }
 }
 
-User.propTypes = {
-  name: PropTypes.string,
-  age: PropTypes.number.isRequired,
-}
+
 export default App;
 
 // VScodeでターミナルを使いたい時「control + shift　+　`を実行。」
