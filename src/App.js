15b92e7440c0b24f563f8745b7a1ee46cbe46cd7
izumichika <chikara0707@outlook.jp>
2020-05-14 19:34:19 +0900

    learn props of component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
 import React　from 'react';
 
-// class App extends Component {
-//   render() {
-//     return(
-//       <React.Fragment>
-//         <label htmlFor="bar">bar</label>
-//         <input type="text" onChange={ () => {console.log("I am clicked")}} />
-//       </React.Fragment>
-//     );
-//   }
-// }
-
 const App = () => {
+  const profiles = [
+    { name: "Taro" , age: 10} , 
+    {name: "Hanako", age: 10},
+    {name: "Hanako"},
+    
+  ]
   return(
     <div>
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
+      {
+        profiles.map((profile, index) => {
+          return <User name={profiles.name} age={profile.age} key={index}/>
+        })
+      }
     </div>
   )
 }
-const Cat = () => {
-  return <div>meow</div>
+const User = (props) => {
+return <div>Hi, I am {props.name}, and {props.age} years old.</div> 
 }
 
+User.defaultProps = {
+  age: 1
+}
 export default App;
 
 // VScodeでターミナルを使いたい時「control + shift　+　`を実行。」
